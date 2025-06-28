f91c52a5666a0a9b021316e608d95dd55e229225
Alfa Renaldo Aluska <alfaaluska@gmail.com>
2025-06-28 23:24:09 +0700

    balikin app.php sala repo bank

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL; // DIINISIALISASI DI KONSTRUKTOR
+    public string $baseURL = 'http://localhost:8080/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -25,11 +25,11 @@ class App extends BaseConfig
      * E.g.,
      * When your site URL ($baseURL) is 'http://example.com/', and your site
      * also accepts 'http://media.example.com/' and 'http://accounts.example.com/':
-     * ['media.example.com', 'accounts.example.com']
+     *     ['media.example.com', 'accounts.example.com']
      *
      * @var list<string>
      */
-    public array $allowedHostnames = []; // BISA TETAP DIBIARKAN KOSONG JIKA TIDAK DARI ENV
+    public array $allowedHostnames = [];
 
     /**
      * --------------------------------------------------------------------------
@@ -40,7 +40,7 @@ class App extends BaseConfig
      * something else. If you have configured your web server to remove this file
      * from your site URIs, set this variable to an empty string.
      */
-    public string $indexPage; // DIINISIALISASI DI KONSTRUKTOR
+    public string $indexPage = 'index.php';
 
     /**
      * --------------------------------------------------------------------------
@@ -51,9 +51,9 @@ class App extends BaseConfig
      * URI string. The default setting of 'REQUEST_URI' works for most servers.
      * If your links do not seem to work, try one of the other delicious flavors:
      *
-     * 'REQUEST_URI': Uses $_SERVER['REQUEST_URI']
+     *  'REQUEST_URI': Uses $_SERVER['REQUEST_URI']
      * 'QUERY_STRING': Uses $_SERVER['QUERY_STRING']
-     * 'PATH_INFO': Uses $_SERVER['PATH_INFO']
+     *    'PATH_INFO': Uses $_SERVER['PATH_INFO']
      *
      * WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
      */
@@ -131,7 +131,7 @@ class App extends BaseConfig
      * dates with the date helper, and can be retrieved through app_timezone()
      *
      * @see https://www.php.net/manual/en/timezones.php for list of timezones
-     * supported by PHP.
+     *      supported by PHP.
      */
     public string $appTimezone = 'UTC';
 
@@ -157,7 +157,7 @@ class App extends BaseConfig
      * secure, the user will be redirected to a secure version of the page
      * and the HTTP Strict Transport Security (HSTS) header will be set.
      */
-    public bool $forceGlobalSecureRequests; // DIINISIALISASI DI KONSTRUKTOR
+    public bool $forceGlobalSecureRequests = false;
 
     /**
      * --------------------------------------------------------------------------
@@ -173,40 +173,30 @@ class App extends BaseConfig
      * the HTTP header for the client IP address.
      *
      * Here are some examples:
-     * [
-     * '10.0.1.200'    => 'X-Forwarded-For',
-     * '192.168.5.0/24' => 'X-Real-IP',
-     * ]
+     *     [
+     *         '10.0.1.200'     => 'X-Forwarded-For',
+     *         '192.168.5.0/24' => 'X-Real-IP',
+     *     ]
      *
      * @var array<string, string>
      */
-    public array $proxyIPs; // DIINISIALISASI DI KONSTRUKTOR (dengan IP Koyeb)
+    public array $proxyIPs = [];
 
     /**
      * --------------------------------------------------------------------------
      * Content Security Policy
      * --------------------------------------------------------------------------
+     *
+     * Enables the Response's Content Secure Policy to restrict the sources that
+     * can be used for images, scripts, CSS files, audio, video, etc. If enabled,
+     * the Response object will populate default values for the policy from the
+     * `ContentSecurityPolicy.php` file. Controllers can always add to those
+     * restrictions at run time.
+     *
+     * For a better understanding of CSP, see these documents:
+     *
+     * @see http://www.html5rocks.com/en/tutorials/security/content-security-policy/
+     * @see http://www.w3.org/TR/CSP/
      */
     public bool $CSPEnabled = false;
-
-
-    // KONSTRUKTOR UNTUK INISIALISASI PROPERTI DARI ENVIRONMENT VARIABLES
-    public function __construct()
-    {
-        parent::__construct(); // WAJIB: Panggil konstruktor parent BaseConfig
-
-        // Inisialisasi baseURL dari environment variable atau default
-        $this->baseURL = env('APP_BASEURL', 'http://localhost:8080/');
-
-        // Inisialisasi indexPage dari environment variable atau default
-        $this->indexPage = env('APP_INDEXPAGE', 'index.php');
-
-        // Untuk keamanan di lingkungan Koyeb (selalu HTTPS)
-        // Ambil dari env APP_FORCE_SECURE, default ke true
-        $this->forceGlobalSecureRequests = (bool) env('APP_FORCE_SECURE', true);
-
-        // IP Proxy untuk Koyeb agar bisa mendeteksi IP asli pengguna
-        // Ini adalah rentang IP private yang umum digunakan oleh provider cloud/container
-        $this->proxyIPs = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '127.0.0.1'];
-    }
 }
