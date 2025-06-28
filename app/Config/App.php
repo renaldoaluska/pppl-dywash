80d0978e62c58bcc22c268830086b4092853f3fb
Alfa Renaldo Aluska <renaldoaluska@gmail.com>
2025-06-28 23:00:23 +0700

    Update App.php
    sesuai KOYEB

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,8 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
-
+    //public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = env('APP_BASEURL', 'http://localhost:8080/'); // Ambil dari env variable APP_BASEUR
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
