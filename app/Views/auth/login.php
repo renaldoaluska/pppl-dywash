c4c73e7571fc9fd21ef1c7810637be8d6875e3db
renaldoaluska <alfaaluska@gmail.com>
2025-06-28 16:28:27 +0700

    header.php footer.php sm login.php include

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login - Dywash</title>
-  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100 min-h-screen flex flex-col">
+<?= $this->include('layout/header') ?>
   <div class="flex-1 flex flex-col justify-center">
     <!-- Logo/Ilustrasi -->
     <div class="flex flex-col items-center bg-blue-50 py-10">
@@ -77,5 +69,4 @@
       </div>
     </div>
   </div>
-</body>
-</html>
+<?= $this->include('layout/footer') ?>
---
+++ app/Views/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?? '- Dywash Laundry' ?></title>
+
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="<?= base_url('assets/img/favicon.ico') ?>">
+</head>
+<body class="bg-gray-100 min-h-screen flex flex-col">
+
+    <nav>
+        </nav>
