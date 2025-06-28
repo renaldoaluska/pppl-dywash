cfc1f797431a2768f5af79901a1322a9f4fd612c
renaldoaluska <alfaaluska@gmail.com>
2025-06-28 16:34:43 +0700

    switch outlet and cust dropdown

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -60,8 +60,8 @@
                         <div class="relative">
                             <select id="role" name="role" required class="appearance-none w-full border rounded-lg px-3 py-3 bg-slate-50 focus:ring-2 focus:ring-indigo-400 transition-all duration-300 outline-none text-slate-700">
                                 <option value="">-- Pilih Role --</option>
-                                <option value="outlet">Outlet</option>
                                 <option value="cust">Customer</option>
+                                <option value="outlet">Outlet</option>
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-slate-400">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
