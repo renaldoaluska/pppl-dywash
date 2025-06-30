41423af3427c426107e1cbe2c633756f4d475850
renaldoaluska <alfaaluska@gmail.com>
2025-06-30 16:28:30 +0700

    favicon to layout

--- app/Views/admin/layout.php
+++ app/Views/admin/layout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="<?= base_url('assets/img/favicon.ico') ?>">
     <title><?= $this->renderSection('title') ?> - DyWash Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
